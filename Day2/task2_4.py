2819932a8e235fa3cf0612bf5581a9ba67721abc
BansiRaiyani43 <bansiraiyani9110@gmail.com>
2025-05-28 15:29:29 +0530

    add file

--- Day2/task2_4.py
+++ Day2/task2_4.py
@@ -5,3 +5,10 @@ A valid pin has:
 	Only numerical characters (0-9).
 	No whitespace.
 """
+def valid_pin(pin):
+    return(len(pin) == 4 or len(pin) == 6) and pin.isdigit()
+
+print(valid_pin("123"))         #false
+print(valid_pin("12345"))       #false
+print(valid_pin("12gh3"))       #false
+print(valid_pin("145623"))      #true
