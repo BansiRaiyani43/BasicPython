05b43d3eb7d44b995d57b8d16477a1026d9d3cfa
BansiRaiyani43 <bansiraiyani9110@gmail.com>
2025-05-28 15:10:11 +0530

    add file

---
+++ Day2/task2_2.py
@@ -0,0 +1,5 @@
+"""
+In cricket, an over consists of six deliveries a bowler bowls from one end. Create a program that takes the number of balls 
+bowled by a bowler and calculates the number of overs and balls bowled by him/her. Return the value as a float, in the format 
+overs.balls.
+"""
---
+++ Day2/task2_4.py
@@ -0,0 +1,7 @@
+# Validate Pin
+"""
+A valid pin has:
+	Exactly 4 or 6 characters.
+	Only numerical characters (0-9).
+	No whitespace.
+"""
