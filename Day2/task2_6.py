19e7d0c687ba8250677ff1e47fbd69d312978608
BansiRaiyani43 <bansiraiyani9110@gmail.com>
2025-05-28 16:07:49 +0530

    add file

---
+++ Day2/task2_6.py
@@ -0,0 +1,9 @@
+#Create a Program that creates product IDs for different fruit juices.
+"""
+Example :
+	("apple", "500ml") ➞ "APP500"
+
+	("pineapple", "45ml") ➞ "PIN45"
+
+	("passion fruit", "750ml") ➞ "PASFRU750"
+"""
