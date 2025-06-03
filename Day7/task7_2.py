0c6c058df0c853c61cb5d4da89bc1c6060f720ac
BansiRaiyani43 <bansiraiyani9110@gmail.com>
2025-06-03 15:54:25 +0530

    add file

--- Day7/task7_2.py
+++ Day7/task7_2.py
@@ -1,1 +1,8 @@
 #Create a function that returns the selected filename from a path. Include the extension in your answer.
+
+import os
+
+def get_filename_from_path(path):
+    return os.path.basename(path)
+
+print(get_filename_from_path("/document/basicpython/day2/task2_2.py"))  
