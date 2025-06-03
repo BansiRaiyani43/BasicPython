28c6f9961fc30479874fb223d530991ab56ee023
BansiRaiyani43 <bansiraiyani9110@gmail.com>
2025-06-03 16:21:27 +0530

    add file

--- Day7/task7_4.py
+++ Day7/task7_4.py
@@ -1,2 +1,11 @@
 # Write a program to read json file and show in terminal.
 # Write a program to dump/write data in json file.
+
+import json
+x = {
+    "name" : "Jhon David",
+    "age" : "28",
+    "city" : "London"
+}
+y = json.dumps(x, indent=6)
+print(y)
