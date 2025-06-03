d23cab2cbc94b2d45a7a2d09065932a866a9cf58
BansiRaiyani43 <bansiraiyani9110@gmail.com>
2025-06-03 16:09:16 +0530

    add changes

--- Day4/task4_4.py
+++ Day4/task4_4.py
@@ -1,17 +1,13 @@
 #Create a function/input that, given a number, returns the corresponding value of that index in the Fibonacci series.
 #The Fibonacci Sequence is the series of numbers:
 #1, 1, 2, 3, 5, 8, 13, 21, 34, ...
-
+  
 def fibonacci(n):
-    if n <= 0:
-        raise ValueError("Index must be a positive integer.")
-    elif n == 1 or n == 2:
+    if n == 0 or n == 1:
         return 1
     a, b = 1, 1
-    for _ in range(3, n + 1):
+    for _ in range(2, n + 1):
         a, b = b, a + b
     return b
-
-print(fibonacci(3))  
-print(fibonacci(7))   
-print(fibonacci(12))  
+print(fibonacci(4))
+print(fibonacci(6))
